f44b1234d094cd3059008adfbd0bc3ee85eb7ea2
aitorSDL <32193889+aitorSDL@users.noreply.github.com>
2018-10-15 12:25:32 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from django.db.models import Model, UUIDField, CharField, EmailField, DateTimeField
-class Clothe(Model):
+from django.db.models import Model, UUIDField, CharField,DateTimeField
+class Clothes(Model):
     id = UUIDField(primary_key=True)
     name = CharField(max_length=45)
     type = CharField(max_length=45)
