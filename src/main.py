7be9cb0b1dd52260116ce43482b87f5dbaa7ccd7
aitorSDL <32193889+aitorSDL@users.noreply.github.com>
2018-10-23 12:13:15 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 
+class ClothesHandler:
+  
+  def login(self,user,passwd)
+    if type(user) != str or type(passwd) != str:
+        return "El usuario no existe."
+    else:
+        return "Usuario OK."
+      
+  def availablePiece(self,id)
+    if type(id) != int:
+        return "ID de pieza inválida."
+    else:
+        print("Buscando pieza...")
