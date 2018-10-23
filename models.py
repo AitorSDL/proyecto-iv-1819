7ce1bea005b201d2fda6bfaa5b55a8a3f41a25bf
aitorSDL <32193889+aitorSDL@users.noreply.github.com>
2018-10-23 11:59:11 +0200

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import Model, UUIDField, CharField,DateTimeField
-class Clothes(Model):
-    id = UUIDField(primary_key=True)
-    name = CharField(max_length=45)
-    type = CharField(max_length=45)
-    price = CharField(max_length=45)
