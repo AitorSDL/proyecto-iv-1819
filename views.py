e42b827ff66ffb4779bc74fcf3f21bc15fae6939
aitorSDL <32193889+aitorSDL@users.noreply.github.com>
2018-10-23 11:58:57 +0200

    fix #15

--- views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from clothes.models import Clothes
-from clothes.serializers import ClothesSerializer
-
-class ClothesViewSet(viewsets.ModelViewSet):
-
-    model = Clothes
-    queryset = Clothes.objects.all()
-    serializer_class = ClothesSerializer
