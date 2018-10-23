a5dc13079bb899439fa0d08efab3007a32b0e4b1
aitorSDL <32193889+aitorSDL@users.noreply.github.com>
2018-10-23 12:42:18 +0200

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(0, '/proyecto-iv-1819/src/')
+
+from main import ClothesHandler
+
+clothesManager = ClothesHandler()
+
+class Tests(object):
+
+  # Comprueba la función login
+  def test_login(self)
+    assert clothesManager.login("user","password") == "Usuario OK."
+    assert clothesManager.login(123,123456) == "El usuario no existe."
+    assert clothesManager.login("testUser","testPass") != "El usuario no existe."
+    
+  # Comprueba la función availablePiece
+  def test_availablePiece(self)
+    assert clothesManager.availablePiece(001) == "ID válida."
+    assert clothesManager.availablePiece("a0012l") == "ID de pieza inválida."
+    assert clothesManager.availablePiece(005) != "ID de pieza inválida."
