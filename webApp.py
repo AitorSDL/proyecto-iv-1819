fc1eff0ae3d1bc7d9b1fbdb53fbafd73a22d01ba
aitorSDL <32193889+aitorSDL@users.noreply.github.com>
2018-10-23 12:41:59 +0200

    Muevo aquí temporalmente para tests Travis

---
+++ webApp.py
@@ -0,0 +1,21 @@
+from flask import Flask, json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "TEST"
+  
+@app.route("/todo")
+def check():
+  with open('status.json') as f:
+    data = json.load(f)
+  response = app.response_class(
+    response=json.dumps(data),
+    status=200,
+    mimetype='application/json'
+  )
+  return response
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
