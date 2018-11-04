aabfc5ffead7bcc3bb2e13982a32f0b241d99573
Aitor <32193889+aitorSDL@users.noreply.github.com>
2018-11-04 19:44:15 +0100

    Create tests.py

---
+++ test/tests.py
@@ -0,0 +1,17 @@
+from src.main import ClothesHandler
+
+clothesManager = ClothesHandler()
+
+class Tests(object):
+
+  # Comprueba la función login
+  def test_login(self):
+    assert clothesManager.login("user","password") == "Usuario OK."
+    assert clothesManager.login(123,123456) == "El usuario no existe."
+    assert clothesManager.login("testUser","testPass") != "El usuario no existe."
+    
+  # Comprueba la función availablePiece
+  def test_availablePiece(self):
+    assert clothesManager.availablePiece(1298347) == "ID válida."
+    assert clothesManager.availablePiece("a0012l") == "ID de pieza inválida."
+    assert clothesManager.availablePiece(111) != "ID de pieza inválida."
