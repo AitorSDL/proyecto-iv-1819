90abcd1adbcdec3e6695da725031f976004a1bde
Aitor <32193889+aitorSDL@users.noreply.github.com>
2018-11-04 19:41:40 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+class ClothesHandler:
+  
+  def login(self,user,passwd):
+    if type(user) != str or type(passwd) != str:
+        return "El usuario no existe."
+    else:
+        return "Usuario OK."
+      
+  def availablePiece(self,pieceId):
+    if type(pieceId) != int:
+        return "ID de pieza inválida."
+    else:
+        print("Buscando pieza...")
+        return "ID válida."
