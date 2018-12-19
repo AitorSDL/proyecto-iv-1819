1de2467657dabc89b80c3310fce72153c09626b8
Aitor <32193889+aitorSDL@users.noreply.github.com>
2018-12-19 12:11:50 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,13 +3,20 @@ class ClothesHandler:
   
   def login(self,user,passwd):
     if type(user) != str or type(passwd) != str:
-        return "El usuario no existe."
+      return "El usuario no existe."
     else:
-        return "Usuario OK."
+      return "Usuario OK."
       
   def availablePiece(self,pieceId):
     if type(pieceId) != int:
-        return "ID de pieza inválida."
+      return "ID de pieza inválida."
     else:
-        print("Buscando pieza...")
-        return "ID válida."
+      print("Buscando pieza...")
+      return "ID válida."
+      
+  def searchPiece(self, itemName):
+    if type(itemName != string:
+      return "Debe escribir un nombre de artículo. Ejemplo: camiseta, pantalón, etc."
+    else:
+      print("Buscando artículo...")
+      return "articulo"
