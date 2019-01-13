6e466ce67bc5ad3156cec197f0890c35cc0b189c
Aitor <32193889+aitorSDL@users.noreply.github.com>
2019-01-13 16:51:03 +0100

    Update webApp.py

--- src/webApp.py
+++ src/webApp.py
@@ -4,22 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+  with open('seller.json') as f:
+    data = json.load(f)
   
-    return "TEST"
+  return data
 
 @app.route("/status")
 def check():
   
   with open('status.json') as f:
     data = json.load(f)
-    
-  response = app.response_class(
-    response=json.dumps(data),
-    status=200,
-    mimetype='application/json'
-  )
   
-  return response
+  return data
   
 
 if __name__ == '__main__':
