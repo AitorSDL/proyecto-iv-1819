6dfdfc4950c687a680f40d3ffa8e5264a1a70728
Aitor <32193889+aitorSDL@users.noreply.github.com>
2019-01-24 18:40:51 +0100

    Update webApp.py

--- src/webApp.py
+++ src/webApp.py
@@ -21,6 +21,18 @@ def check():
   
   return jsonify(status='OK')
   
+@app.route("/warehouse")
+def item_list():
+  with open('warehouse.json') as f:
+    data = json.load(f)
+  
+  response = app.response_class(
+    response=json.dumps(data),
+    status=200,
+    mimetype='application/json'
+  )
+  
+  return response
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
