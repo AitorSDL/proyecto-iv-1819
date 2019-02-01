822bf6e2857537e6ded13870b0557c28c54b856c
Aitor <32193889+aitorSDL@users.noreply.github.com>
2019-02-01 13:04:41 +0100

    Update fabfile.py

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -1,1 +1,22 @@
+from fabric.api import *
+
+
+def init_service():
+
+     # Iniciamos el servicio web
+    
+     run('cd proyecto-iv-1819/ && sudo gunicorn app:__hug_wsgi__ -b 0.0.0.0:80')
+      
+      
+def update_service():
+
+    # Eliminamos la instalación actual para prevenir posible problemas
+    run('rm -rf proyecto-iv-1819')
+
+    # Descargamos desde el repositorio
+    run('git clone https://github.com/AitorSDL/proyecto-iv-1819.git')
+
+    # Instalamos las dependencias mediante el fichero requirements.txt
+    run('pip3 install -r proyecto-iv-1819/requirements.txt')
+
 
