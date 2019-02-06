ac82b2db5b5076eb4c1b0dbb0b38169c59f3a236
Aitor <32193889+aitorSDL@users.noreply.github.com>
2019-02-06 15:22:21 +0100

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -21,13 +21,7 @@ def check():
   with open('status.json') as f:
     data = json.load(f)
   
-  response = app.response_class(
-    response=json.dumps(data),
-    status=200,
-    mimetype='application/json'
-  )
-  
-  return response
+  return data
   
 @app.route("/warehouse")
 def item_list():
